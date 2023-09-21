e6089572cf6353ddf649fd38e89f10da96f23b96
GAURAV RAJPUT <56871623+grvrjt@users.noreply.github.com>
2023-09-21 18:22:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const dbConn = require('./dbConnection');
 const port = process.env.PORT||3000;
 const app = express();
 app.use(bodyParser.json());
-dbConn.dbConnection((err,dbResult)=>{
-    if(err){
-        console.log(err)
-    }else{
-        app.listen(port)
-        console.log(`${dbResult} and server is listening at port ${port}`);
+// dbConn.dbConnection((err,dbResult)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         app.listen(port)
+//         console.log(`${dbResult} and server is listening at port ${port}`);
 
-    }
-})
+//     }
+// })
 
 app.get('/',(req,res)=>{
     res.send(`Hello from server`)
